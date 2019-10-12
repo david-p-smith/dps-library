6a44fc75a3866daef788e19ba8341105a396bc9a
DPS <smith.dp@gmail.com>
2019-10-12 12:17:01 +0100

    start express listening on port 3000

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello from my library app');
 });
+
+app.listen(3000, function () {
+    console.log('Express listening on port 3000');
+});
