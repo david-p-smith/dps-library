b18e4cd8ca0949163a80a2d1b8ab0641559d8a9f
DPS <smith.dp@gmail.com>
2019-10-12 15:37:07 +0100

    set views on app

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+// templating config
+app.set('views', './src/views');
 
 // get requests / routing
 app.get('/', (req, res) => {
