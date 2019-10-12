4d594fff71126b4f713b964a5e25fc5f78df5bdf
DPS <smith.dp@gmail.com>
2019-10-12 12:44:06 +0100

    require path, root listener serve index html

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug')('app');
 var morgan = require('morgan');
+var path = require('path');
 
 var app = express();
 
@@ -10,7 +11,7 @@ app.use(morgan('tiny'));
 
 //get requests / routing
 app.get('/', function (req, res) {
-    res.send('Hello from my library app');
+    res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
 app.listen(3000, function () {
