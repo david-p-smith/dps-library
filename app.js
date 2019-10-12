905ce9b500cf96d2634a43c493d07cf9fef3a5be
DPS <smith.dp@gmail.com>
2019-10-12 13:10:42 +0100

    add jquery npm js config to express

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 //get requests / routing
 app.get('/', function (req, res) {
