b0503605202708f353b83a1fa68d2c90debeb75b
DPS <smith.dp@gmail.com>
2019-10-14 20:40:50 +0100

    book route middleware example

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -64,6 +64,12 @@ function router(nav) {
     });
 
   bookRouter.route('/:id')
+    .all((req, res, next) => {
+      // middleware here
+      // do processing
+      // add values needed in get to req object.  e.g. rec.book = recordset
+      next();
+    })
     .get((req, res) => {
       const { id } = req.params;
       res.render(
