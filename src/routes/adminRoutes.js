81466da63d8f90e418dd46fa6e0f56a7f39bd4c5
DPS <smith.dp@gmail.com>
2019-10-17 21:26:49 +0100

    insert books into mongo

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,12 +1,82 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:adminRoutes');
 
 const adminRouter = express.Router();
+
+// books
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'Les Misérables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false
+  },
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: false
+  },
+  {
+    title: 'A Journey into the Centre of the Earth',
+    genre: 'Science Fiction',
+    author: 'Jules Verne',
+    read: false
+  },
+  {
+    title: 'The Dark World',
+    genre: 'Fantasy',
+    author: 'Henry Kuttner',
+    read: false
+  },
+  {
+    title: 'The Wind in the Willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    read: false
+  },
+  {
+    title: 'Life of a Development Manager',
+    genre: 'Reality',
+    author: 'David Smith',
+    read: true
+  }
+
+];
 
 function router(nav) {
   adminRouter.route('/')
     .get((req, res) => {
-      res.send('inserting books');
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+
+        let client;
+
+        try {
+
+          client = MongoClient.connect(url);
+          debug('Connected correctly to Mongo server');
+
+          const db = client.db(dbName);
+
+          const response = await db.collection('books').insertMany(books);
+          res.json(response);
+
+        } catch (err) {
+          debug(err.stack);
+        }
+
+        client.close();
+      }());
     });
 
   return adminRouter;
