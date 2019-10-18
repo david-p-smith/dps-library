476add3db57186135eac4ca5719e84fdfa6c59c7
DPS <smith.dp@gmail.com>
2019-10-18 21:32:18 +0100

    select books from mongo

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,66 +1,43 @@
 const express = require('express');
+const debug = require('debug')('app:bookRoutes');
 
 const bookRouter = express.Router();
 
 function router(nav) {
-  // books
-  const books = [
-    {
-      title: 'War and Peace',
-      genre: 'Historical Fiction',
-      author: 'Lev Nikolayevich Tolstoy',
-      read: false
-    },
-    {
-      title: 'Les Misérables',
-      genre: 'Historical Fiction',
-      author: 'Victor Hugo',
-      read: false
-    },
-    {
-      title: 'The Time Machine',
-      genre: 'Science Fiction',
-      author: 'H. G. Wells',
-      read: false
-    },
-    {
-      title: 'A Journey into the Centre of the Earth',
-      genre: 'Science Fiction',
-      author: 'Jules Verne',
-      read: false
-    },
-    {
-      title: 'The Dark World',
-      genre: 'Fantasy',
-      author: 'Henry Kuttner',
-      read: false
-    },
-    {
-      title: 'The Wind in the Willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Grahame',
-      read: false
-    },
-    {
-      title: 'Life of a Development Manager',
-      genre: 'Reality',
-      author: 'David Smith',
-      read: true
-    }
-
-  ];
 
   // routing
   bookRouter.route('/')
     .get((req, res) => {
-      res.render(
-        'bookListView',
-        {
-          title: 'Library',
-          nav,
-          books
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to Mongo server');
+
+          const db = client.db(dbName);
+
+          const col = await db.collection('books');
+
+          const books = await col.find().toArray();
+
+          res.render(
+            'bookListView',
+            {
+              title: 'Library',
+              nav,
+              books
+            }
+          );
+        } catch (err) {
+          debug(err.stack);
         }
-      );
+
+        client.close();
+      }());
     });
 
   bookRouter.route('/:id')
