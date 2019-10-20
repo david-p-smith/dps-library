ed904569f32c9cb9e99a2e0a4dfe3d35165b7c71
DPS <smith.dp@gmail.com>
2019-10-20 14:07:58 +0100

    serialise user

--- src/config/passport.js
+++ src/config/passport.js
@@ -9,9 +9,11 @@ module.exports = function passportConfig(app) {
   app.use(passport.session());
 
   // Stores user in session
-  passport.serializeUser(
+  passport.serializeUser((user, done) => {
 
-  );
+    // skeleton - done(err, thing)
+    done(null, user); //could use just user.id for efficiency
+  });
 
   // Retrieves user from session
   passport.deserializeUser(
