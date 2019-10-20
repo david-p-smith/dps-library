e0bb8d595d7e2325ad23eac438e8a6be81726790
DPS <smith.dp@gmail.com>
2019-10-20 13:43:24 +0100

    debug post request

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,10 +5,11 @@ const debug = require('debug')('app:authRoutes');
 const authRouter = express.Router();
 
 function router() {
-    authRouter.route('/signUp')
-        .post((req, res) => {
-
-        });
+  authRouter.route('/signUp')
+    .post((req, res) => {
+      debug(req.body);
+    });
+  return authRouter;
 };
 
 module.exports router;
