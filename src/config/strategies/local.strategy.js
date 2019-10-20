4bffea149a87444bcd76bc968ef12fdea230d587
DPS <smith.dp@gmail.com>
2019-10-20 14:20:05 +0100

    create new strategy

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -2,5 +2,14 @@ const passport = require('passport');
 const { Strategy } = require('passport-local');
 
 module.exports = function localStrategy() {
-
+  passport.use(new Strategy(
+    {
+      usernameField: username,
+      passwordField: password
+    }, (username, password, done) => {
+      const user = {
+        username, password
+      };
+    }
+  ));
 };
