432ba16a4255bb3d0fd502e38769f2ab91dc85f8
DPS <smith.dp@gmail.com>
2019-10-23 22:40:36 +0100

    book show call to goodreads

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,11 +1,27 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
 
+const parser = xml2js.Parser({ explicitArray: false });
 
 function goodreadsService() {
   function getBookById(bookId) {
     return new Promise((resolve, reject) => {
-      resolve({
-        description: 'our description'
-      });
+      axios.get('https://www.goodreads.com/book/show/656.xml?key=HHm6qh6cO85LcwzLjMxAgg')
+        .then((response) => {
+          parser.parseString(response.data, (err, result) => {
+            if (err) {
+              debug(err);
+            } else {
+              debug(result);
+              resolve(result.GoodreadsResponse.book);
+            }
+          });
+        })
+        .catch((error) => {
+          reject(error);
+          debug(error);
+        });
     });
   }
 
